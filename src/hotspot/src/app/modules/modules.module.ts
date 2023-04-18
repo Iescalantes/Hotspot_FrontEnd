a586efc6699b77b9588d599e1aad94b50f7f1e08
iescalantes <alum.iescalantes@iesalixar.org>
2023-04-18 18:38:18 +0200

    creacion login register

--- src/hotspot/src/app/modules/modules.module.ts
+++ src/hotspot/src/app/modules/modules.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { RegistrationComponent } from './registration/registration.component';
 import { HomeComponent } from './home/home.component';
 import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
 
 
 
 @NgModule({
   declarations: [
     RegistrationComponent,
-    HomeComponent
+    HomeComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
